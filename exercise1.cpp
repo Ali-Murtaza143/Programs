a9e905992a01765a4b36ace953b7a417277d1f9f
Ali Murtaza <58934980+Ali-Murtaza143@users.noreply.github.com>
2020-09-20 23:27:55 +0500

    Name of array - Pointer

--- exercise1.cpp
+++ exercise1.cpp
@@ -2,27 +2,21 @@
 
 using namespace std;
 
-main ()
+int main ()
 {
-    int arr[3][3];
-    int xCoordinate, yCoordinate;
+    int arr[5];
+    int *pArr = arr;
 
-    arr[0][0] = 5;
-    arr[0][1] = 3;
-    arr[0][2] = 2;
-    arr[1][0] = 1;
-    arr[1][1] = 4;
-    arr[1][2] = 15;
-    arr[2][0] = 123;
-    arr[2][1] = 12;
-    arr[2][2] = 42;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Input " << i+1 << " number: ";
+        cin >> *(pArr++);
+    }
 
-    cout << "Input x coordinate: ";
-    cin >> xCoordinate;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Number " << i+1 << ": " << arr[i] << endl;
+    }
 
-    cout << "Input y coordinate: ";
-    cin >> yCoordinate;
-
-    cout << arr[xCoordinate-1][yCoordinate-1];
-
+    return 0;
 }
