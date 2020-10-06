da4436e0b8be952eeaa096954f13768595c6e8a1
Ali Murtaza <58934980+Ali-Murtaza143@users.noreply.github.com>
2020-10-07 01:24:45 +0500

    Add files via upload

--- exercise6.cpp
+++ exercise6.cpp
@@ -1,124 +1,23 @@
 #include <iostream>
-#include <cstdlib>
+
 
 using namespace std;
 
-void chooseOperation(char);
-void addition();
-void subtraction();
-void multiplication();
-void division();
-void exponentiation();
-
-
-int main ()
+main()
 {
-    while (true)
-    {
-        char choice;
+    double firstElement, secondElement, divisionResult;
 
-        cout << "What mathematical operation you want to use ?" << endl;
-        cout << "Type: '+' - addition, '-' - subtraction, '*' - multiplication, '/' - division, '^' - exponentiation: ";
-        cin >> choice;
+    cout << "This is a simple division program." << endl;
+    cout << "Input first number: ";
+    cin >> firstElement;
+    cout << "Input second number: ";
+    cin >> secondElement;
 
-        system("cls");
-        chooseOperation(choice);
-    }
-    return 0;
-}
-
-void chooseOperation(char choice)
-{
-    switch(choice)
-    {
-        case '+':
-            addition();
-            break;
-        case '-':
-            subtraction();
-            break;
-        case '*':
-            multiplication();
-            break;
-        case '/':
-            division();
-            break;
-        case '^':
-            exponentiation();
-            break;
-    }
-}
-
-void addition()
-{
-    double firstNumber, secondNumber, result;
-
-    cout << "Input the first number to add: ";
-    cin >> firstNumber;
-    cout << "Input the second number to add: ";
-    cin >> secondNumber;
-
-    result = firstNumber + secondNumber;
-    cout << firstNumber << " + " << secondNumber << " = " << result << endl;
+    divisionResult = firstElement / secondElement;
+    cout << "First number: " << firstElement << endl;
+    cout << "Second number: " << secondElement << endl;
+    cout << "Division result: " << divisionResult << endl;
 
 }
 
-void subtraction()
-{
-    double firstNumber, secondNumber, result;
 
-    cout << "Input the number to subtract from: ";
-    cin >> firstNumber;
-    cout << "Input the number to subtract with: ";
-    cin >> secondNumber;
-
-    result = firstNumber - secondNumber;
-    cout << firstNumber << " - " << secondNumber << " = " << result << endl;
-
-}
-
-void multiplication()
-{
-    double firstNumber, secondNumber, result;
-
-    cout << "Input the first number: ";
-    cin >> firstNumber;
-    cout << "Input the number to multiply by: ";
-    cin >> secondNumber;
-
-    result = firstNumber * secondNumber;
-    cout << firstNumber << " * " << secondNumber << " = " << result << endl;
-
-}
-
-void division()
-{
-    double firstNumber, secondNumber, result;
-
-    cout << "Input the first number: ";
-    cin >> firstNumber;
-    cout << "Input the number to divide by: ";
-    cin >> secondNumber;
-
-    result = firstNumber / secondNumber;
-    cout << firstNumber << " / " << secondNumber << " = " << result << endl;
-
-}
-
-void exponentiation()
-{
-    double base, exponent, result = 1;
-
-    cout << "Input the base to exponentiate: ";
-    cin >> base;
-    cout << "Input the exponent value: ";
-    cin >> exponent;
-
-    for (int i = 0; i < exponent; i++)
-    {
-        result *= base;
-    }
-
-    cout << "The number " << base << " to the power of " << exponent << " is equal to " << result << "." << endl;
-
-}
--- exercise7.cpp
+++ exercise7.cpp
@@ -1,51 +1,23 @@
 #include <iostream>
-#include <cstdlib>
+
 
 using namespace std;
 
-double minValue(double tab[]);
-double maxValue(double tab[]);
-
-
-int main ()
+main()
 {
-    double tab[5];
+    int firstElement, secondElement, divisionRemainder;
 
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "Input " << i+1 << " number: ";
-        cin >> tab[i];
-    }
+    cout << "This is a simple program that calculates the remainder of division of two numbers." << endl;
+    cout << "Input first number: ";
+    cin >> firstElement;
+    cout << "Input second number: ";
+    cin >> secondElement;
 
-    cout << "The minimum value was: " << minValue(tab) << endl;
-    cout << "The maximum value was: " << maxValue(tab) << endl;
+    divisionRemainder = firstElement % secondElement;
+    cout << "First number: " << firstElement << endl;
+    cout << "Second number: " << secondElement << endl;
+    cout << "Remainder: " << divisionRemainder << endl;
 
-    return 0;
 }
 
-double minValue(double tab[])
-{
-    double minValue = tab[0];
 
-    for (int i = 1; i < 5; i++)
-    {
-        if (minValue > tab[i])
-            minValue = tab[i];
-
-    }
-
-    return minValue;
-}
-double maxValue(double tab[])
-{
-    double maxValue = tab[0];
-
-    for (int i = 1; i < 5; i++)
-    {
-        if (maxValue < tab[i])
-            maxValue = tab[i];
-
-    }
-
-    return maxValue;
-}
